ffc64454b784deb58ae7cc7305b89d1f61897a4c
Caption-D <drumilhved@gamil.com>
2023-03-07 01:35:21 +0530

    did add relation ship between blog and blog data like likes and comments, image part yet to be added to the Blog Schema and also need to add image photo for publisher

--- api/database.js
+++ api/database.js
@@ -1,4 +1,5 @@
 const { timeStamp } = require("console");
+const { Schema } = require("mongoose");
 const mongoose = require("mongoose");
 const mongodbUrl = process.env.DATABASE_URL;
 
@@ -7,10 +8,26 @@ const mongodbUrl = process.env.DATABASE_URL;
 mongoose.connect(mongodbUrl,{useNewUrlParser: true});
 
 const BlogSchema = new mongoose.Schema({
-    userId: String,
+
     time: timeStamp,
     auther: String,
     title: String,
-    content: String
-
-})
+    content: String,
+    OtherData: {
+        type: Schema.Types._id,
+        ref: 'BlogData'
+    }
+});
+//made relationship between Blog schema and Other BLog Data which will have 
+//data like comments , saves , shares etc.
+module.exports = mongoose.model('Blog',BlogSchema);
+const OtherBlogData = new mongoose.Schema({
+    likes: Number,
+    comments: String,
+    saves: Number,
+    BlogPost: {
+        type: Schema.Types._id,
+        ref: 'Blog'
+    }
+});
+module.exports = mongoose.model("BlogData", OtherBlogData);
