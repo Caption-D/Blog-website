18d1909723cd4927c14773b492f34f961572a532
Caption-D <drumilhved@gamil.com>
2023-03-07 12:26:41 +0530

    added basic schema and tested it out, now to run the database.js use node or nodemon database.js and then do the testing as it won't update changes when just saved, currnetly the schema and database are woeking in the backed

--- api/database.js
+++ api/database.js
@@ -2,32 +2,53 @@ const { timeStamp } = require("console");
 const { Schema } = require("mongoose");
 const mongoose = require("mongoose");
 const mongodbUrl = process.env.DATABASE_URL;
-
+const testMongoUrl = "mongodb://localhost:27017/blog";
 //connecting to mongoDB online database
-
-mongoose.connect(mongodbUrl,{useNewUrlParser: true});
+//actual link while in production
+// mongoose.connect(mongodbUrl,{useNewUrlParser: true});
+//testing link 
+mongoose.connect(testMongoUrl,{useNewUrlParser: true});
 
 const BlogSchema = new mongoose.Schema({
-
-    time: timeStamp,
+    // time: timeStamp,
     auther: String,
     title: String,
     content: String,
-    OtherData: {
-        type: Schema.Types._id,
-        ref: 'BlogData'
-    }
+    likes: Number,
+    comments: [{type: String}],
+    saves: Number,
+    view: Number,
+    //Relationship
+        // Autehr: {
+        // type: mongoose.Schema.Types._id,
+        // ref: 'Auther'
+        // }
+    
+},{timestamps: true});
+const Blog = mongoose.model('blog',BlogSchema);
+const blog1 = new Blog({
+    // time: Date.now(),
+    auther: "Drumil HV",
+    title: "TEst",
+    content: "We are testing now and we hope to see that it passes, if it does we will soon add other features like auther profile his posts, for now just under testing",
+    likes: 10,
+    comments: ['lets hope it happens', "lets go!!"],
+    saves:10,
+    view:20,
 });
+blog1.save();
+module.exports = mongoose.model("Blog", BlogSchema);
 //made relationship between Blog schema and Other BLog Data which will have 
 //data like comments , saves , shares etc.
-module.exports = mongoose.model('Blog',BlogSchema);
-const OtherBlogData = new mongoose.Schema({
-    likes: Number,
-    comments: String,
-    saves: Number,
-    BlogPost: {
-        type: Schema.Types._id,
-        ref: 'Blog'
-    }
-});
-module.exports = mongoose.model("BlogData", OtherBlogData);
+
+
+// add this section later when testing and basic protoypt is done 
+//do not change the name given below as they could contain important data like 
+//relationship with the Blog tabel
+//================================================>
+        // const BlogAuther = new mongoose.Schema({
+        //     Name: String,
+        //         //once this is done we need to add profile photo and blog photos
+        //     profilePhoto: 
+        // })
+        //module.exports = mongoose.modle('Auther', BlogAuther);
--- api/server.js
+++ api/server.js
@@ -1,5 +1,8 @@
 const express  = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+// const fs = require('fs');
+// const path = require('path');
 const mongodbUrl = process.env.DATABASE_URL;
 const app = express();
 app.get("/", function(req, res){
